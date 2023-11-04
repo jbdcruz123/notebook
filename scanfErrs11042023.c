f1988cf61b4cf4ab1cea20a3d4a142d4df18a75d
Ron S. Medrano <83150978+jbdcruz123@users.noreply.github.com>
2023-11-04 11:32:27 +0800

    Create scanfErrs11042023.c

---
+++ scanfErrs11042023.c
@@ -0,0 +1,71 @@
+
+
+//Example of input and output in C
+//always use newline drainer
+#include<stdio.h>
+
+void fflush_( void );
+
+void fflush_( void ){//2
+      char c;
+      int I=1;
+      while(  c != '\n' ){//3
+            c = getchar( );
+            printf( "\nI %d, c %c, d %d", I, c, c );
+            I+=1;
+      }//3
+      
+}//2
+
+
+int main(void ){//2
+
+      char ch;
+      char s[10];
+      int n;
+      float fl;
+      
+      while( ch != 'q' && ch != 'Q' ){//2
+      
+            printf("\n\ninput int: " );
+            scanf( "%d", &n ); 
+             // ex input 1   will newline stream
+             //ex: input ab 
+             // n = 0 and "ab\n" = and on  stream 
+             //ex: input  12ab , 
+             // 12 =n, "ab\n" = on stream
+            
+            fflush_( );                              
+            printf("\ninput str: " );
+            scanf( "%s", s ); //newline in 2 string
+            /* ex: input ab  newline on stream
+                 ex: input   ab   cd   
+                 s = "ab" and  " cd\n" = on stream
+            */
+            
+            fflush_( );
+                        
+            printf("\ninput float: " );
+            scanf( "%f", &fl );  //same as int
+            
+            fflush_( );
+      
+            printf( "\n\ndisplay all value\nint %d\nstr %s\n%0.2f\n", n, s, fl);                                                      
+            printf("press q to leave: " );
+      
+            scanf( "%c", &ch ); 
+            /* example input blank result ch = '\n' and none on stream
+         
+             input 'a'  result ch ='a' and '\n' = on stream       
+            */
+        
+            
+            if( ch != 10 ) {//3
+            
+                  fflush_( );
+            }//3
+              
+      }//2
+      
+      return 0;
+}//2
